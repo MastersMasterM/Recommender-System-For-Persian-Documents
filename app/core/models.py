90310d19a98c2d5b0b48a84e2230fe22da825c55
M4stersMasterm <mo.mahdi1379@gmail.com>
2023-08-15 14:57:57 +0330

    DB schema defined

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,16 @@
-from django.db import models # noqa
+"""
+Defining Database Schema
+"""
+from django.db import models
 
-# Create your models here.
+
+class Paper(models.Model):
+    uid = models.CharField(max_length=12)
+    title = models.TextField()
+    abstract = models.TextField()
+    fl_subject = models.TextField()
+    sl_subject = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+    
