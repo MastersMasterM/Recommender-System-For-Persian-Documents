dc1e0fd635d0620e75db55c5977ba30380edebe5
M4stersMasterm <mo.mahdi1379@gmail.com>
2023-08-16 19:35:50 +0330

    Task reverted

--- app/get_paper/urls.py
+++ app/get_paper/urls.py
@@ -3,5 +3,5 @@ from .views import GetPaperView
 
 urlpatterns = [
     path('get_paper/<int:pk>', GetPaperView.as_view(), name='get_paper'),
-    path('get_paper', GetPaperView.as_view(), name='all_papers')
+    path('get_paper/', GetPaperView.as_view(), name='all_papers')
 ]
