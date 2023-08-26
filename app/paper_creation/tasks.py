1e16fe06eaf9130cbe83b600c9e078b6e9ac929e
M4stersMasterm <mo.mahdi1379@gmail.com>
2023-08-26 21:36:53 +0330

    New Method Added

--- app/paper_creation/tasks.py
+++ app/paper_creation/tasks.py
@@ -1,12 +1,21 @@
 """
-Defining the task of recomputing recommendations based on db
+Eporting Data into a CSV File
 """
 from time import sleep
 from celery import shared_task
-
+from core.models import Paper
+import csv
 
 @shared_task
-def recompute_embeddings():
-    print('Recomputing Recommendation started....')
-    sleep(10)
-    print('Embeddings computed')
+def export_paper_table():
+    print('Exporting Data Started...')
+    papers = Paper.objects.all()
+
+    with open('paper_data.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['َuid', 'title', 'abstract', 'fl_subject', 'sl_subject'])  # Add column headers
+
+        for paper in papers:
+            writer.writerow([paper.uid, paper.title, paper.abstract, paper.fl_subject, paper.sl_subject])  # Write data rows
+
+    print('Data export completed...')
--- app/paper_creation/views.py
+++ app/paper_creation/views.py
@@ -7,16 +7,32 @@ from rest_framework.permissions import IsAuthenticated
 
 from paper_creation.serializers import PaperSerializer
 from core.models import Paper
-from .tasks import recompute_embeddings
+from .tasks import export_paper_table
 
 
 class PaperViewSet(ModelViewSet):
-    """View for manage recipe APIs"""
+    """View for managing paper APIs"""
     serializer_class = PaperSerializer
     queryset = Paper.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        last_paper = Paper.objects.order_by('-id').first()  # Get the last paper object
+
+        if last_paper is not None:
+            new_id = last_paper.id + 1  # Increment the last primary key value by one
+            serializer.save(id=new_id)  # Save the new object with the updated primary key
+        else:
+            serializer.save()  # Save the new object using the default primary key generation
+
+        # Execute the task after the object has been saved
+        export_paper_table.delay()
+
     def create(self, request, *args, **kwargs):
-        recompute_embeddings.delay()
-        return super().create(request, *args, **kwargs)
+        response = super().create(request, *args, **kwargs)
+
+        # Execute the task after the object has been created
+        export_paper_table.delay()
+
+        return response
